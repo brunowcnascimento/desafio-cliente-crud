b50a265d0fa01892a37e45b01baf59bef5171b67
Bruno Nascimento <brunowcnascimento@gmail.com>
2024-11-24 19:24:50 -0300

    Implementando o find by id com a exception

--- src/main/java/com/devsuperior/ClienteCRUD/service/ClientService.java
+++ src/main/java/com/devsuperior/ClienteCRUD/service/ClientService.java
@@ -3,6 +3,7 @@ package com.devsuperior.ClienteCRUD.service;
 import com.devsuperior.ClienteCRUD.entity.Client;
 import com.devsuperior.ClienteCRUD.entity.dto.ClientDTO;
 import com.devsuperior.ClienteCRUD.repository.ClientRepository;
+import com.devsuperior.ClienteCRUD.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,5 +20,11 @@ public class ClientService {
     public Page<ClientDTO> findAll(Pageable pageable) {
         Page<Client> result = repository.findAll(pageable);
         return result.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client result = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));;
+        return new ClientDTO(result);
     }
 }
