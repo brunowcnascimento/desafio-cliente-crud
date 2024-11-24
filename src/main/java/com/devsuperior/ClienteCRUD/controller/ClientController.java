a99a803ac4f303d10039fb55cbe0bd2ad1539ce8
Bruno Nascimento <brunowcnascimento@gmail.com>
2024-11-24 15:45:26 -0300

    Implementando o find All

---
+++ src/main/java/com/devsuperior/ClienteCRUD/controller/ClientController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.ClienteCRUD.controller;
+
+import com.devsuperior.ClienteCRUD.entity.dto.ClientDTO;
+import com.devsuperior.ClienteCRUD.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/client")
+public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+}
