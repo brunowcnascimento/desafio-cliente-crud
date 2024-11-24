b218b43aa4d4c9c935ded8c33f98b4c4192cc9f4
Bruno Nascimento <brunowcnascimento@gmail.com>
2024-11-24 15:15:58 -0300

    Implementando o ClientDTO

---
+++ src/main/java/com/devsuperior/ClienteCRUD/entity/dto/ClientDTO.java
@@ -0,0 +1,55 @@
+package com.devsuperior.ClienteCRUD.entity.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+    private Long id;
+
+    @NotEmpty
+    private String nome;
+    private String cpf;
+    private Double income;
+
+    @PastOrPresent
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO(Long id, String nome, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public @NotEmpty String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public @PastOrPresent LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
